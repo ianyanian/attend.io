8b514ff383a1ee22d1d9cdd59959bd9e153960e5
Ian Renaldo <ianyanian@users.noreply.github.com>
2018-01-28 10:33:02 -0500

    Add files via upload

---
+++ attend-bot/functions/commands/appointments.js
@@ -0,0 +1,24 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* /hello
+*
+*
+*   See https://api.slack.com/slash-commands for more details.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+  callback(null, {
+    text: `2 events scheduled for this week:\n- Team Building Seminar - 14:00 - Friday, January 28th, 2018\n- Roll out to Production - 15:00 - Friday, January 28th, 2018`,
+    attachments: [
+      // You can customize your messages with attachments.
+      // See https://api.slack.com/docs/message-attachments for more info.
+    ]
+  });
+};
---
+++ attend-bot/functions/commands/remindsmsgroup2.js
@@ -0,0 +1,29 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* /hello
+*
+*
+*   See https://api.slack.com/slash-commands for more details.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+  lib.utils.sms['@1.0.4']({
+		to: '6479096437',
+		body: 'Reminder: Team Building Seminar - 14:00 - Friday, January 28th, 2018'
+	}, (err, result) => {
+		callback(null, {
+      text: `SMS sent to @Trevor and 3 others.`,
+      attachments: [
+        // You can customize your messages with attachments.
+        // See https://api.slack.com/docs/message-attachments for more info.
+      ]
+    });
+	});
+};
